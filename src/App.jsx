08f21377c5b763ac04c2ea9dc5e618e0a6b7c862
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-06 17:01:24 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from "./Pages/Layout";
 import HomePage from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
-import ContactPage from "./Pages/Contact";
-import LoginPage from "./Pages/LoginPage";
-import SignUpPage from "./Pages/SignUpPage";
+import Contact from "./Pages/Contact";
+import LoginPage from "./Pages/Login";
+import SignUpPage from "./Pages/SignUp";
 import DetailsPage from "./Pages/Details";
 import NotFoundPage from "./Pages/404 Page/NotFoundPage";
 
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="about-us" element={<AboutUs />} />
-          <Route path="contact" element={<ContactPage />} />
+          <Route path="contact" element={<Contact />} />
           <Route path="details" element={<DetailsPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignUpPage />} />
