1a8d3e5b99b47c59e3e0133a360f3b6d4ebaa97d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-06 16:58:00 +0100

    Create Contact.jsx

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Contact = () => {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  // }
+
+
+  return (
+    <section>
+      <div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Contact
