9c12f97c64d57174e90f16e435c543874f44e420
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-06 16:58:40 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,46 @@ export default {
   ],
   theme: {
     extend: {},
+    backgroundImage: {
+      // 'header-image': 'url(https://placekitten.com/1400)'
+      'header-image': 'url(../../src/assets/header-BG.png)'
+    },
+
+    // colors: {
+    //   'co-habit': '#010886',
+    // },
+
+    screens: {
+      'smaller': '350px',
+      // formerly 450
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+      'midlaptop': '740px',
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      notbold: '620',
+      almostbold: '650',
+      bold: '700',
+      extrabold: '800',
+      'extra-bold': '800',
+      black: '900',
+    }
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 }
-
