8595a94ca3952bbcf319be3bde4fb3bd7bc716cb
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-06 16:58:09 +0100

    Create NavbarData.jsx

---
+++ src/Components/Navbars/NavbarData.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const NavbarData = [
+  {
+    title: "Home",
+    path: "/",
+    className: "",
+  },
+
+  {
+    title: "About Us",
+    path: "/about-us",
+    className: "",
+  },
+
+  {
+    title: "Login",
+    path: "/login",
+    className: "laptop:hidden flex",
+  },
+  {
+    title: "Sign Up",
+    path: "/signup",
+    className: "laptop:hidden flex",
+  }
+];
