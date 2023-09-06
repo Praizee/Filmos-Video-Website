2d6813ec52b250f677346ae3b5f16a2a5016b355
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-06 16:58:25 +0100

    Create AboutUs.jsx

---
+++ src/Pages/AboutUs.jsx
@@ -0,0 +1,37 @@
+//<br className="hidden laptop:block" />
+import React from 'react'
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const animationConfiguration = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
+
+const AboutUs = () => (
+  <section className=" bg-[#010886]">
+
+    <motion.div
+      variants={animationConfiguration}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 3 }}
+    >
+      {/* <div className='min-h-screen'> */}
+      <div className="hero place-content-start ">
+
+
+      </div>
+      {/* END OF OUR STORY */}
+
+
+    </motion.div>
+
+
+  </section>
+)
+
+export default AboutUs
