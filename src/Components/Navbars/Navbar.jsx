7824018b06fc36f7bc5fcd1fbc699fac1f2fdc7a
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-07 10:00:40 +0100

    Update Navbar.jsx

--- src/Components/Navbars/Navbar.jsx
+++ src/Components/Navbars/Navbar.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { NavbarData } from "./NavbarData"
+import BlackLogo from "../../assets/5ffffae014844edabd6b8c5c_logo-black.svg"
 
 const Navbar = () => {
   const [showNav, setshowNav] = useState(false)
 
   return (
-    <div className="navbar bg-white border-none fixed top-0 -mt-1 z-[100]">
+    <div className="navbar bg-white border-none fixed py-8 top-0 -mt-1 z-[100]">
       <div className="navbar-start laptop:ml-[6.5rem]">
-        <a>
-          {/* <img className="logo" src={CoHabit} alt="web Logo" /> */}
+        <a className="leading-[1.5rem] text-black font-black text-[1.5rem]">
+          <img className="w-[60%]" src={BlackLogo} alt="web Logo" />
         </a>
       </div>
 
@@ -21,7 +22,7 @@ const Navbar = () => {
               <li key={index} className="">
                 {/* "flex min-w-max p-4" */}
                 <NavLink to={item.path} className={({ isActive }) =>
-                  isActive ? 'flex min-w-max rounded-none mx-2 pb-[0.3rem] border-b-[3px] px-1 border-[#010886]' : 'font-medium min-w-max px-1 mx-2'
+                  isActive ? 'flex min-w-max rounded-none mx-2 pb-[0.3rem] border-b-[3px] px-1 border-[black]' : 'font-medium min-w-max px-1 mx-2'
                 }>
                   <span className={item.className}>{item.title}</span>
                 </NavLink>
@@ -49,7 +50,7 @@ const Navbar = () => {
                 <li key={index} className="">
                   {/* "flex min-w-max p-4" */}
                   <NavLink to={item.path} className={({ isActive }) =>
-                    isActive ? 'flex min-w-max rounded-none font-semibold focus:bg-white p-2 border-b-[3px] border-[#305D96]' : 'flex rounded-none focus:bg-white min-w-max py-2 px-2'
+                    isActive ? 'flex min-w-max rounded-none font-semibold focus:bg-white p-2 border-b-[3px] border-[black]' : 'flex rounded-none focus:bg-white min-w-max py-2 px-2'
                   }>
                     <span className="">{item.title}</span>
                   </NavLink>
@@ -61,21 +62,14 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-end laptop:mr-[7.5rem] hidden laptop:flex">
+      {/* <div className="navbar-end laptop:mr-[7.5rem] hidden laptop:flex">
         <Link to="/login" className="btn bg-white hover:bg-white border-none mr-1 normal-case text-[#010886]">
           Login
         </Link>
-        {/* <img src={Line} className="ml-0 mr-2" alt="line" /> */}
+        <img src={ } className="ml-0 mr-2" alt="line" />
         <Link to="/signup" className="btn py-2 px-6 normal-case font-bold bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">
           Sign Up
         </Link>
-      </div>
-
-
-      {/* <div className="navbar-end laptop:mr-[7.5rem] hidden laptop:flex">
-        <a className="btn bg-white hover:bg-white border-none mr-[0.009rem] normal-case text-[#010886]">Login</a>
-        <img src={Line} className="ml-0 mr-2" alt="line" />
-        <a className="btn py-2 px-6 normal-case font-bold bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Sign Up</a>
       </div> */}
 
     </div>
