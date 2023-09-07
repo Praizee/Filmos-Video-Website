2a21806bfa1b8cbc4b33685372c51d92c7fbd7ab
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-07 13:13:23 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import Palm from "../assets/palm-frond.jpeg"
+import ChainFence from "../assets/chain-fence.jpg"
+import IceSkating from "../assets/ice-skating.jpg"
+import Shrug from "../assets/shrug.jpeg"
+import Eye from "../assets/eye.jpg"
+import Freckles from "../assets/freckles.jpg"
+import Boat from "../assets/boat.jpg"
+import RugLady from "../assets/rug-lady.jpg"
+import PillowLady from "../assets/pillow-lady.jpg"
+import Thinking from "../assets/thinking.jpg"
 
 
 const animationConfiguration = {
@@ -13,7 +23,7 @@ const Home = () => {
   const [index, setIndex] = useState(0);
 
   return (
-    <section className="">
+    <section className="text-black bg-white">
       <motion.div
         variants={animationConfiguration}
         initial="initial"
@@ -22,22 +32,21 @@ const Home = () => {
         transition={{ duration: 3 }}
       >
 
-        <div className="mt-12 min-h-screen laptop:mx-[7rem] ">
+        <div className=" laptop:mt-[20rem] border h-[4rem] p-4 laptop:p-0 mt-[10rem] min-h-screen mb-0 laptop:mx-[7rem] ">
           {/* bg-[#f8f8fd] */}
           <div className="">
-            <h1 class="text-[9rem]">
+            <h1 class="laptop:text-[9rem] laptop:leading-[8rem] font-black">
               Video  <br />
               production.
             </h1>
-
 
           </div>
 
         </div>
 
-        <div className=" bg-[#f8f8fd]">
+        <div className="border-2 mt-0">
           <div className="laptop:mx-[7rem] h-60">
-
+            <img src="" className="" alt="" />
           </div>
 
         </div>
