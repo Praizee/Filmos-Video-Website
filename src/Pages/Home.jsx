5271c5c40a0debc4d9a5b39ffa5d40e799fd35f5
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-07 16:08:48 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
 import Palm from "../assets/palm-frond.jpeg";
 import ChainFence from "../assets/chain-fence.jpg";
 import IceSkating from "../assets/ice-skating.jpg";
@@ -12,6 +11,19 @@ import RugLady from "../assets/rug-lady.jpg";
 import PillowLady from "../assets/pillow-lady.jpg";
 import Thinking from "../assets/thinking.jpg";
 import Video1 from "../assets/agency-video.mp4";
+
+const imageStyles = [
+  "w-1/2",         // Style for the first image
+  "w-1/2",         // Style for the second image
+  "w-1/2",         // Style for the third image
+  "w-1/2",         // Style for the fourth image
+  "w-1/2",         // Style for the fifth image
+  "w-1/2",         // Style for the sixth image
+  "w-1/2",         // Style for the seventh image
+  "w-1/2",         // Style for the eighth image
+  "w-1/2",         // Style for the ninth image
+  "w-1/2",         // Style for the tenth image
+];
 
 const animationConfiguration = {
   initial: { opacity: 0 },
@@ -31,34 +43,30 @@ const Home = () => {
         exit="exit"
         transition={{ duration: 3 }}
       >
-        <div className=" laptop:mt-[20rem] border-4 h-[4rem] p-4 laptop:p-0 mt-[10rem] min-h-screen mb-0 laptop:mx-[7rem] ">
-          {/* bg-[#f8f8fd] */}
-          <div className="">
+        <div className="laptop:mt-[20rem] border-4 h-[4rem] p-4 laptop:p-0 mt-[10rem] min-h-screen mb-0 laptop:mx-[7rem] ">
+          <div>
             <h1 className="laptop:text-[9rem] laptop:leading-[8rem] font-black">
-              Video  <br />
-              production.
+              Video <br /> production.
             </h1>
-
           </div>
-
         </div>
 
         <div className="my-20 mx-4 laptop:mx-0">
           <div className="laptop:mx-[7rem]">
             {[Palm, ChainFence, IceSkating, Shrug, Eye, Freckles, Boat, RugLady, PillowLady, Thinking].map((image, index) => (
-              <div key={index} className="relative w-[50%] my-2">
+              <div key={index} className={`relative ${imageStyles[index]} my-2`}>
                 <div
-                  className="relative"
+                  className="relative group"
                   onMouseEnter={() => setHoveredVideo(index)}
                   onMouseLeave={() => setHoveredVideo(null)}
                 >
                   <img
                     src={image}
-                    className="laptop:w-1/2 my-2 w-auto cursor-pointer"
+                    className="laptop:w-[] w-full my-2 cursor-pointer"
                     alt=""
                   />
                   {hoveredVideo === index && (
-                    <div className="">
+                    <div className="absolute inset-0 flex items-center justify-center group-hover:opacity-100 opacity-0 transition-opacity duration-500">
                       <video
                         src={Video1}
                         className="w-full h-full object-cover"
@@ -67,16 +75,15 @@ const Home = () => {
                         muted
                         preload="auto"
                       />
-                      <div className="absolute bottom-0 left-0 right-0 bg-transparent text-[1.rem] font-bold text-white text-center py-[6rem]">
+                      <div className="text-container text-center absolute text-white">
                         <motion.h1
                           initial={{ opacity: 0, y: 20 }}
                           animate={{ opacity: 1, y: 0 }}
                           transition={{ duration: 0.5 }}
+                          className="text-[1.5rem] font-bold"
                         >
                           Common Ground
                         </motion.h1>
-                      </div>
-                      <div className="absolute bottom-0 left-0 right-0 bg-transparent text-white text-center py-[3rem]">
                         <motion.p
                           initial={{ opacity: 0, y: 20 }}
                           animate={{ opacity: 1, y: 0 }}
