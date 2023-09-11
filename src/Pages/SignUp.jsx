1027e2f7e298b0c662d4f04d4f4ef54c817dd85a
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-09-11 10:55:35 +0100

    Update SignUp.jsx

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -23,8 +23,6 @@ const Signup = () => {
           <div className="hero-content text-center">
             <div className="max-w-lg">
               <h1 className=" text-[3.125rem] leading-[3.4375rem] font-bold uppercase text-[#25324B]">SIGN UP</h1>
-              {/* <h1 className="text-5xl font-bold leading-[3.4375rem] text-[#010886]">SHARED APARTMENT</h1>
-              <button className="py-2 px-6 font-bold text-[1.125rem] bg-[#010886f1] hover:bg-[#010886] text-white hover:text-white">Search</button> */}
             </div>
           </div>
         </motion.div>
